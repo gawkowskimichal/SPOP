3efff5841602ebdd6f26a361b76f091846c2da53
Michal Gawkowski <gawkowskimichal@gmail.com>
2014-06-01 23:13:37 +0200

    zainspirowany początek

--- interface.hs
+++ interface.hs
@@ -12,3 +12,24 @@ displayInterface :: IO()
 saveToFile :: FilePath -> IO()
 
 loadFromFile :: FilePath -> IO()
+
+
+type Board = Array Int Piece
+
+data Piece = Piece PieceType
+
+data PieceType = Owca | Wilk
+
+
+
+emptyBoard :: Board
+emptyBoard = array (0,63) []
+
+piece :: Char -> Piece
+piece c = Piece pType
+pType = case (toUpper c) of
+'O' -> Owca
+'W' -> Wilk
+
+addToBoard :: Piece -> Int -> Board -> Board
+addToBoard piece loc board = board
