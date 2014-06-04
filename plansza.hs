a9a4a2c92b6ed8d5d75d539f8fd3765208d019a5
Michal Gawkowski <gawkowskimichal@gmail.com>
2014-06-04 14:20:10 +0200

    update planszy

--- plansza.hs
+++ plansza.hs
@@ -8,10 +8,10 @@ data PieceType = Owca | Wilk deriving Eq
 
 type FilePath = String
 
-printBoard::Board->String
+printBoard::Board-> String
 printBoard  = unlines . map (concatMap prettySquare)
 
-printSquare::Square->String
+printSquare::Square-> String
 printSquare Nothing = "-- "
 printSquare (Just (Piece a)) = show a ++ " "
 
@@ -19,25 +19,33 @@ instance Show PieceType where
  show Owca = "O"
  show Wilk = "W"
 
-isEmpty::Board->Pos->Bool
+isEmpty::Board-> Pos-> Bool
 isEmpty board pos = Nothing == getSquare board pos
 
 emptySquare::Square
 emptySquare = Nothing
 
-getSquare::Board->Pos->Square
+getSquare::Board-> Pos-> Square
 getSquare board (a, b) = board!!a!!b
 
 updateBoard::Pos-> Square-> Board-> Board
 updateBoard = updateMatrix
 
-deleteSquare::Pos->Board->Board
+deleteSquare::Pos-> Board-> Board
 deleteSquare p = updateBoard p emptySquare
 
-movePos::Pos->Pos->Board->Board
+movePos::Pos-> Pos-> Board-> Board
 movePos p1 p2 b = updateBoard p2 (getSquare b p1) (deleteSquare p1 b)
 
-outside,inside::Pos->Bool
+updateList::[a]->Int->(a->a)->[a]
+updateList [] _ _ = []
+updateList (x:xs) 0 f = (f x):xs
+updateList (x:xs) n f = x:updateList xs (n-1) f
+
+updateMatrix::(Int, Int)->a->[[a]]->[[a]]
+updateMatrix (i,j) a m = updateList m i (\z->updateList z j (const a))
+
+outside,inside::Pos-> Bool
 outside (a, b) = a < 0 || b < 0 || a > 7 || b > 7
 inside = not . outside
 
@@ -51,7 +59,7 @@ initialBoard = [[Nothing, Just (Piece Owca), Nothing, Just (Piece Owca), Nothing
                 [Nothing, Nothing, Nothing, Nothing, Nothing, Nothing, Nothing, Nothing],
                 [Just (Piece Wilk), Nothing, Nothing, Nothing, Nothing, Nothing, Nothing, Nothing]]
 
-emptyBoard = [[Nothing|_<-[1..8]]|_<-[1..8]]
+emptyBoard = [[Nothing|_<- [1..8]]|_<- [1..8]]
 
 displayState :: IO()
 
